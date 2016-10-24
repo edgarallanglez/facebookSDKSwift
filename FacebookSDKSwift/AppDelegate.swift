ce1169288817b5aa70e4d916221f25f95a02d742
edgarallanglez <edgarallanglez@gmail.com>
2016-10-24 13:23:04 -0600

    first commit for Swift FB SDK config

--- FacebookSDKSwift/AppDelegate.swift
+++ FacebookSDKSwift/AppDelegate.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import FacebookCore
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private let delegate: FBSDKApplicationDelegate = FBSDKApplicationDelegate.sharedInstance()
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    
+    public func application(_ application: UIApplication,
+                            didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        return delegate.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return delegate.application(application, open:url, sourceApplication:sourceApplication, annotation:annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        AppEventsLogger.activate(application)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- FacebookSDKSwift/ViewController.swift
+++ FacebookSDKSwift/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        // Handle clicks on the button
+//        loginButton.addTarget(self, action: @selector(self.loginButtonClicked) forControlEvents: .TouchUpInside)
+
+        if let accessToken = AccessToken.current {
+            // User is logged in, use 'accessToken' here.
+        }
+        
+        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // Once the button is clicked, show the login dialog
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+            }
+        }
+    }
 
 }
 
